fc0b8b4c68e85c105511df4d105f77838bafa9b2
CordobaBenjamin <cordobabenjamin37@gmail.com>
2024-04-23 04:44:35 -0300

    fix error with vercel push 0.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { useMode } from "@/app/css/colors Switcher/modeSwitcher";
 import Header from "@/app/components/index/indexComponents/header";
 import Footer from "@/app/components/index/indexComponents/footer";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ModeProvider } from "@/app/css/colors Switcher/modeSwitcher";
 import "./css/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +15,7 @@ export const metadata: Metadata = {
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
       <div className={`bg-bodyBg`}>
-        <ModeProvider>{children}</ModeProvider>
+        {children}
       </div>
   );
 };
